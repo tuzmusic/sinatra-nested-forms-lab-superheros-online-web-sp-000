5039da9103c8145806856cce01bb3d1d332ab1ff
Jonathan Tuzman <tuzmusic@gmail.com>
2018-11-19 15:09:06 +0000

    automatically backed up by learn

---
+++ app/models/team.rb
@@ -0,0 +1,12 @@
+class Team
+
+  attr_accessor :name, :power, :bio
+
+  def initialize(hash)
+    hash.each do |key, value|
+      self.send("#{key}=", value) if value.class != Array
+    end
+    self
+  end
+
+end
